b8a3b7ef93fc58a841e29244c2b9b2dfbc80a0be
Xavierlhm <xavier3102lee@gmail.com>
2025-01-21 13:04:48 +0800

    Added codes to echo user input

--- src/main/java/Tracker.java
+++ src/main/java/Tracker.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class Tracker {
+    private static final String HORIZONTAL_LINE = "    ____________________________________________________________";
+
     public static void greet() {
-        String message = "____________________________________________________________\n" +
-                "Hello! I'm Tracker\nWhat can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!";
+        String message = "    Hello! I'm Tracker\n    What can I do for you?";
+        System.out.println(HORIZONTAL_LINE);
         System.out.println(message);
+        System.out.println(HORIZONTAL_LINE);
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         greet();
+
+        String input;
+        do {
+            input = scanner.nextLine();
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("    " + input);
+                System.out.println(HORIZONTAL_LINE);
+            }
+        } while (!input.equalsIgnoreCase("bye"));
+
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
     }
 }

bff972fb3e277944cae20ccec7ea1352f7e5a583
Xavierlhm <xavier3102lee@gmail.com>
2025-01-21 13:02:22 +0800

    Renamed Duke.java to Tracker.java and added codes to rename, greet, exit

---
+++ src/main/java/Tracker.java
@@ -0,0 +1,13 @@
+public class Tracker {
+    public static void greet() {
+        String message = "____________________________________________________________\n" +
+                "Hello! I'm Tracker\nWhat can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!";
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
